f83f132ae47f9b17a34157aaf6e274548ef543a7
Simon Anderson <simon.anderson111@gmail.com>
2020-05-06 07:28:55 +1000

    Keyboard not working, need to fix

--- main.lua
+++ main.lua
@@ -2,22 +2,57 @@
 -- Push library is used to make virtual size windows, i.e. makes the pixels bigger.
 push = require 'push'
 
-WINDOW_WIDTH = 1280
+WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
 
 -- Override this to initialise the sate at the begining of program execution
 function love.load()
     -- nearest-neighbor filter downscaling:
     love.graphics.setDefaultFilter('nearest','nearest')
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+    -- retro-looking font:
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    
+    -- score font:
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    -- active font:
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {fullscreen = false})
+
+    -- initialise scores:
+    player1Score = 0
+    player2Score = 0
+
+    -- initialise paddle positions:
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 -- love.keypressed(key) Executes the command associated with 'key'
@@ -29,13 +64,32 @@ end
 
 function love.draw()
     -- begin rendering using virtual resolution:
-    push:apply('start')
+    push:start()
 
-    -- profile love.graphics.printf(text, x, y, [width], [align])
-    love.graphics.printf('Hello Isamu!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')               
+    -- set grey background:
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    -- draw welcome text toward the top of the screen
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- draw score on the left and right center of the screen
+    -- need to switch font to draw before actually printing
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+    -- render first paddle (left side)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- render second paddle (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball (center)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end virtual resolution:
-    push:apply('end')
+    push:finish()
 end
 
 
