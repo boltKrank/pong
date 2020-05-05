d0904af20077749014c76b79899e4f5970a0c86f
Simon Anderson <simon.anderson111@gmail.com>
2020-05-06 06:49:12 +1000

    Added push resolution

--- main.lua
+++ main.lua
@@ -1,38 +1,41 @@
--- The screen width variables
+-- https://github.com/Ulydev/push
+-- Push library is used to make virtual size windows, i.e. makes the pixels bigger.
+push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Override this to initialise the sate at the begining of program execution
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- nearest-neighbor filter downscaling:
+    love.graphics.setDefaultFilter('nearest','nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
--- Call each frame after update to draw things
-function love.draw()
-    -- profile love.graphics.printf(text, x, y, [width], [align])
-    love.graphics.printf(
-        'Hello Isamu!',         -- Text to display
-        0,                      -- Starting X 
-        WINDOW_HEIGHT / 2 - 6,  -- Starting halfway down the screen
-        WINDOW_WIDTH,           -- Number of pixels to center within
-        'center')               -- alignment mode, can be 'center', 'left', or right.
+-- love.keypressed(key) Executes the command associated with 'key'
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
--- Called each frame by Love. dt is the time in seconds since last frame
--- dt is deltaTime used to scale.
--- function love.update(dt){
+function love.draw()
+    -- begin rendering using virtual resolution:
+    push:apply('start')
+
+    -- profile love.graphics.printf(text, x, y, [width], [align])
+    love.graphics.printf('Hello Isamu!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')               
+
+    -- end virtual resolution:
+    push:apply('end')
+end
 
 
--- }
-
-
-
--- like the C command printf it puts text on the screen
-
-
--- love.window.setMode(width, height, params) initialises the Window's dimensions
